ab3b8955a0827d478ca26a574b85494b8e81efd2
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2021-11-09 16:47:51 +0100

    fsharp docker example

---
+++ fsharp/fhello/Program.fs
@@ -0,0 +1,13 @@
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s %s" whom (Environment.GetEnvironmentVariable("part"))
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+    0 // return an integer exit code
