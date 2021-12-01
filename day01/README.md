42b5594f49e078480ff06875e0b002ecbb158f16
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2021-12-01 08:28:07 +0100

    da01 for real

--- day01/README.md
+++ day01/README.md
@@ -1,13 +1,12 @@
-# F# (.NET Core 5.0)
-F# is a functional language in the ML (Meta Language) tradition, whose main feature is pattern matching in patterns that simultaneously do destructuring assignments, as well as really good type inference. It has been proved that a well-typed ML program cannot get a runtime type error. F# runs on the .NET CLR, so you can also access everything from the standard CLR libraries. F# is also known for having support for units of measure and the pipeline syntax, similar to unix shell pipes.
+# Fortran (gfortran = Fortran95 with some additions)
+Fortran is a language that lives on because it is really good at doing maths fast, like for huge astrophysics equations. Fortran's facilities for working with arrays are outstanding. The Fortran compiler can often generate code that is faster than hand-crafted assembler and it is usually faster than C code.
 
-To run the code locally, you will need to install [.NET 5.0](https://dotnet.microsoft.com/download/dotnet/5.0)
-
-## How to run tests
-Open a terminal in this directory and type `dotnet test`
+The compiler gfortran comes bundled with gcc which is readily available if you don't have it already.
 
 ## How to build and run
-To run locally, just type `dotnet run`
+If you have many source files, all except the main program first need to be compiled separately, e.g. `gfortran -c a.f90` will create `a.o`. The you compile the object files together with your program, e.g. `gfortran myprog.f a.o b.o`
+
+If you didn't give it another name with the `-o` option to the compiler, your program is run as `./a.out`
 
 ## Docker
 According to the [Cygni AoC procedure](https://github.com/cygni/aoc_example)
--- day01/main.f90
+++ day01/main.f90
@@ -1,51 +1,19 @@
-function solutionPart1(n, data, is_prime)
+function solutionPart1(n, data)
   implicit none
-  integer :: n, data(0:n-1), solutionPart1, i
-  logical :: is_prime(n)
-  integer, allocatable :: indices(:)
-  indices = pack([(i, i=0,n-1)], is_prime)
-  solutionPart1=sum(data(indices) * indices)
+  integer :: n, data(n), solutionPart1
+  solutionPart1=count(data(2:n).gt.data(1:n-1))
 end function
 
-function solutionPart2(n, data, is_prime)
+function solutionPart2(n, data)
   implicit none
-  integer :: n, data(n), solutionPart2, signed(n), i, s
-  logical :: is_prime(n)
-  s = 1
-  do i = 1, n
-    signed(i) = s * data(i)
-    s = -1 * s
-  end do
-  solutionPart2=sum(pack(signed, .not.is_prime))
+  integer :: n, data(n), solutionPart2, sliding(n-2)
+  sliding=data(1:n-2)+data(2:n-1)+data(3:n)
+  solutionPart2=count(sliding(2:n-2).gt.sliding(1:n-3))
 end function
-
-function isPrime(n)
-  implicit none
-  integer :: n, p
-  logical :: isPrime
-  do p = 2, int(sqrt(real(n)))
-    if (mod(n, p) == 0) then
-      isPrime = .false.
-      return
-    end if
-  end do
-  isPrime = .true.
-end function
-
-subroutine findPrimes(n, data, is_prime)
-  implicit none
-  integer :: n, data(n), i
-  logical, intent(out) :: is_prime(n)
-  logical :: isPrime
-  do i = 1, n
-    is_prime(i) = isPrime(data(i))
-  end do
-end subroutine
 
 program aoc
   implicit none
   integer, allocatable :: data(:)
-  logical, allocatable :: is_prime(:)
   integer :: n, stat, x
   character(len=6) :: part
   integer :: solutionPart1, solutionPart2
@@ -61,14 +29,12 @@ program aoc
   rewind(20)
   read(20,*)data
   close(20)
-  allocate(is_prime(n))
-  call findPrimes(n, data, is_prime)
 
   call get_environment_variable('part', part)
   if (trim(part).eq.'part1'.or.trim(part).eq.'') then
-    print *, solutionPart1(n, data, is_prime)
+    print *, solutionPart1(n, data)
   else if (trim(part).eq.'part2') then
-    print *, solutionPart2(n, data, is_prime)
+    print *, solutionPart2(n, data)
   else
     print *, 'Unknown part '//part
   end if
