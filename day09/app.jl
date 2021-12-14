1e3ca60aac46279cd46fb04ff1f468b21e2cb45d
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2021-12-14 16:46:42 +0100

    minor tweak

--- day09/app.jl
+++ day09/app.jl
@@ -1,8 +1,6 @@
 input = vcat([permutedims(parse.(Int8, l)) for l in collect.(readlines("input.txt"))]...)
 include("solutions.jl")
 
-println("Julia")
-
 part = get(Base.ENV, "part", "part1")
 println(if part == "part1"
   solutionpart1()
