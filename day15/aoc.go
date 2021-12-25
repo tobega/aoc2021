ff59b7263856ddc467b93fcec06b2cdbbf79f1b9
Torbjörn Gannholm <torbjorn.gannholm@gmail.com>
2021-12-25 21:54:54 +0100

    day15

--- day15/aoc.go
+++ day15/aoc.go
@@ -9,18 +9,60 @@ import (
 )
 
 func getSolutionPart1(cave [][]int) int {
-	costs := make([][]int, len(cave))
-	for i,_ := range costs {
-	  costs[i] = make([]int, len(cave[0]))
-		for j,_ := range costs[i] {
+	height := len(cave)
+	width := len(cave[0])
+	costs := make([][]int, height)
+	for i := range costs {
+		costs[i] = make([]int, width)
+		for j := range costs[i] {
 			costs[i][j] = math.MaxInt32
 		}
 	}
-	return 0
+	i := 0
+	j := 0
+	costs[0][0] = 0
+	for j < width {
+		if i == height {
+			j++
+			i = 0
+			continue
+		}
+		if i > 0 && costs[i-1][j]+cave[i][j] < costs[i][j] {
+			costs[i][j] = costs[i-1][j] + cave[i][j]
+		}
+		if j > 0 && costs[i][j-1]+cave[i][j] < costs[i][j] {
+			costs[i][j] = costs[i][j-1] + cave[i][j]
+		}
+		if i > 0 && costs[i][j]+cave[i-1][j] < costs[i-1][j] {
+			costs[i-1][j] = costs[i][j] + cave[i-1][j]
+			i--
+			continue
+		}
+		if j > 0 && costs[i][j]+cave[i][j-1] < costs[i][j-1] {
+			costs[i][j-1] = costs[i][j] + cave[i][j-1]
+			j--
+			continue
+		}
+		i++
+	}
+	return costs[height-1][width-1]
 }
 
 func getSolutionPart2(cave [][]int) int {
-	return 0
+	height := len(cave)
+	width := len(cave[0])
+	bigcave := make([][]int, height*5)
+	for i := range bigcave {
+		cavei := i % height
+		addi := i / height
+		bigcave[i] = make([]int, width*5)
+		for j := range bigcave[i] {
+			cavej := j % width
+			addj := j / width
+			bigcave[i][j] = (cave[cavei][cavej]+addi+addj-1)%9 + 1
+		}
+	}
+	return getSolutionPart1(bigcave)
 }
 
 func parseInput(input string) [][]int {
@@ -31,7 +73,7 @@ func parseInput(input string) [][]int {
 	for _, line := range lines {
 		var row []int
 		for _, risk := range []rune(line) {
-		  row = append(row, int(risk - '0'))
+			row = append(row, int(risk-'0'))
 		}
 		cave = append(cave, row)
 	}
